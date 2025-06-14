002da3b254c5c37c1eac2876b1a9729946e7cd19
Pau <96744558+paurosellfarre@users.noreply.github.com>
2025-06-14 10:56:21 +0200

    Add SearchTripsUseCase and integrate with trip routes for trip searching functionality

---
+++ src/application/usecases/SearchTripsUseCase.js
@@ -0,0 +1,80 @@
+const { ValidationError } = require('../../shared/errors/AppErrors');
+const Trip = require('../../domain/entities/trip');
+
+/**
+ * Use case for searching trips
+ */
+class SearchTripsUseCase {
+  /**
+   * @param {ITripService} tripService - Service for fetching trips
+   * @param {RedisCache} cacheService - Cache service for storing results
+   */
+  constructor(tripService, cacheService) {
+    this.tripService = tripService;
+    this.cacheService = cacheService;
+  }
+  /**
+   * Searches for trips from an origin to a destination with sorting
+   * @param {string} origin - IATA code for origin
+   * @param {string} destination - IATA code for destination
+   * @param {string} sortBy - Sorting strategy ('fastest' or 'cheapest')
+   * @returns {Promise<Trip[]>} Sorted array of trips
+   * @throws {ValidationError} If parameters are invalid
+   */
+  async execute(origin, destination, sortBy) {
+    
+    
+    // Validate sort parameter
+    if (sortBy !== 'fastest' && sortBy !== 'cheapest') {
+      throw new ValidationError("Sort parameter must be either 'fastest' or 'cheapest'");
+    }
+    
+    // Validate locations before calling the API
+    if (!this.tripService.isLocationSupported(origin)) {
+      throw new ValidationError(`Origin ${origin} is not supported`);
+    }
+
+    if (!this.tripService.isLocationSupported(destination)) {
+      throw new ValidationError(`Destination ${destination} is not supported`);
+    }
+
+    // Generate cache key
+    const cacheKey = `trips:${origin}:${destination}`;
+
+    // Try to get from cache
+    const cachedResults = await this.cacheService.get(cacheKey);
+    if (cachedResults) {
+      let trips = JSON.parse(cachedResults);
+
+      // Transform cached trips to Trip entities
+      trips = trips.map(tripData => Trip.create(tripData));
+      return this.sortTrips(trips, sortBy);
+    }
+
+    // Fetch from service if not in cache
+    const trips = await this.tripService.searchTrips(origin, destination);
+    
+    // Save to cache
+    await this.cacheService.set(cacheKey, JSON.stringify(trips));
+
+    // Sort and return results
+    return this.sortTrips(trips, sortBy);
+  }
+
+  /**
+   * Sorts trips based on strategy
+   * @private
+   * @param {Trip[]} trips - Array of trips to sort
+   * @param {string} sortBy - Sorting strategy
+   * @returns {Trip[]} Sorted array of trips
+   */
+  sortTrips(trips, sortBy) {
+    if (sortBy === 'fastest') {
+      return [...trips].sort((a, b) => a.duration - b.duration);
+    } else {
+      return [...trips].sort((a, b) => a.cost - b.cost);
+    }
+  }
+}
+
+module.exports = SearchTripsUseCase;
---
+++ src/presentation/routes/tripRoutes.js
@@ -0,0 +1,204 @@
+const express = require('express');
+const ValidationMiddleware = require('../middlewares/validationMiddleware');
+const { createRateLimiter } = require('../middlewares/rateLimiter');
+
+// Import use cases
+const SearchTripsUseCase = require('../../application/usecases/SearchTripsUseCase');
+const SaveTripUseCase = require('../../application/usecases/SaveTripUseCase');
+const ListTripsUseCase = require('../../application/usecases/ListTripsUseCase');
+const DeleteTripUseCase = require('../../application/usecases/DeleteTripUseCase');
+
+// Import controller
+const TripController = require('../controllers/TripController');
+
+// Import services and repositories
+const ExternalTripAPIService = require('../../infrastructure/services/ExternalTripAPIService');
+const RedisCache = require('../../infrastructure/cache/redisCache');
+const RedisTripRepository = require('../../infrastructure/repositories/RedisTripRepository');
+
+// Create instances
+const redisCache = new RedisCache();
+const tripService = new ExternalTripAPIService();
+const tripRepository = new RedisTripRepository(redisCache);
+
+// Create use cases
+const searchTripsUseCase = new SearchTripsUseCase(tripService, redisCache);
+const saveTripUseCase = new SaveTripUseCase(tripRepository);
+const listTripsUseCase = new ListTripsUseCase(tripRepository);
+const deleteTripUseCase = new DeleteTripUseCase(tripRepository);
+
+// Create controller
+const tripController = new TripController(
+  searchTripsUseCase,
+  saveTripUseCase,
+  listTripsUseCase,
+  deleteTripUseCase
+);
+
+// Create rate limiters
+const rateLimiterMiddleware = createRateLimiter({
+  windowMs: 60 * 1000, // 1 minute window
+  max: 30 // 30 requests per minute
+});
+
+// Create router
+const router = express.Router();
+
+/**
+ * @swagger
+ * /api/trips/search:
+ *   get:
+ *     summary: Search for trips from origin to destination
+ *     description: Search for trips with sorting options (fastest or cheapest)
+ *     parameters:
+ *       - in: query
+ *         name: origin
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: IATA 3-letter code for origin
+ *       - in: query
+ *         name: destination
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: IATA 3-letter code for destination
+ *       - in: query
+ *         name: sort_by
+ *         schema:
+ *           type: string
+ *           enum: [fastest, cheapest]
+ *           default: cheapest
+ *         description: Sorting strategy
+ *     responses:
+ *       200:
+ *         description: List of trips
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Trip'
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ *       500:
+ *         description: Server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
+router.get(
+  '/search',
+  rateLimiterMiddleware,
+  ValidationMiddleware.validateSearchTrips,
+  (req, res, next) => tripController.searchTrips(req, res, next)
+);
+
+/**
+ * @swagger
+ * /api/trips:
+ *   post:
+ *     summary: Save a trip
+ *     description: Add a new trip to the saved trips list
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Trip'
+ *     responses:
+ *       201:
+ *         description: Trip saved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Trip'
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ *       500:
+ *         description: Server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
+router.post(
+  '/',
+  rateLimiterMiddleware,
+  ValidationMiddleware.validateSaveTrip,
+  (req, res, next) => tripController.saveTrip(req, res, next)
+);
+
+/**
+ * @swagger
+ * /api/trips:
+ *   get:
+ *     summary: List all saved trips
+ *     description: Get a list of all saved trips
+ *     responses:
+ *       200:
+ *         description: List of saved trips
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Trip'
+ *       500:
+ *         description: Server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
+router.get(
+  '/',
+  rateLimiterMiddleware,
+  (req, res, next) => tripController.listTrips(req, res, next)
+);
+
+/**
+ * @swagger
+ * /api/trips/{id}:
+ *   delete:
+ *     summary: Delete a trip
+ *     description: Delete a trip by its ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Trip ID
+ *     responses:
+ *       204:
+ *         description: Trip deleted successfully
+ *       404:
+ *         description: Trip not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ *       500:
+ *         description: Server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
+router.delete(
+  '/:id',
+  rateLimiterMiddleware,
+  (req, res, next) => tripController.deleteTrip(req, res, next)
+);
+
+module.exports = router;
